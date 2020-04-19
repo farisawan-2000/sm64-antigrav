a659ebc6af3ceeb7a59015babb57e2d232e32e16
farisawan-2000 <faris@SIRAF2.localdomain>
2020-04-19 15:43:59 -0400

    puppycam added

---
+++ levels/bob/header.inc.h
@@ -0,0 +1,8 @@
+extern const GeoLayout bob_area_1_MY_LEVEL[];
+extern const GeoLayout bob_area_1_level[];
+extern const Collision bob_area_1_collision[];
+extern const MacroObject bob_area_1_MY_AREA_macro_objs[];
+extern const Gfx bob_Cube_mesh[];
+extern const Gfx bob_Cylinder_mesh[];
+extern const Gfx bob_material_revert_render_settings[];
+
---
+++ levels/bob/texture_include.inc.c
@@ -0,0 +1,3 @@
+const u8 bob_close_i8[] = {
+	#include "levels/bob/close.i8.inc.c"
+};
